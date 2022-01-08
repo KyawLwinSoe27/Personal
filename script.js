d70d28aba03ca6c5a97846cccf1d7d8f19cb0832
Kyaw Lwin Soe <51574554+KyawLwinSoe27@users.noreply.github.com>
2022-01-08 21:34:37 +0630

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+let firstName = document.getElementById("firstName");
+let lastName = document.getElementById("lastName");
+let bYear = document.getElementById("bYear");
+let major = document.getElementById("major");
+let uni = document.getElementById("uni");
+let sub = document.getElementById("sub");
+let btn = document.getElementById("btn");
+let key = document.getElementById("key")
+
+date = new Date();
+currentYear = date.getFullYear();
+
+
+
+let year = bYear.valueAsDate
+let birthYear = year.getFullYear();
+
+let age = currentYear - birthYear;
+
+function answer(){
+    key.innerText = `My name is ${firstName.value} ${lastName.value} and I'm ${age} years old.
+    I'm ${major.value} student. I study at ${uni.value} University. I'm interested in ${sub.value}.`;
+}
+
+
