c56df8566f6a683083cd364104fb4208152a082b
Kyaw Lwin Soe <51574554+KyawLwinSoe27@users.noreply.github.com>
2022-01-09 18:33:52 +0630

    Update script.js

--- script.js
+++ script.js
@@ -7,10 +7,19 @@ let sub = document.getElementById("sub");
 let btn = document.getElementById("btn");
 let key = document.getElementById("key")
 
-date = new Date();
-currentYear = date.getFullYear();
+let date = new Date();
 
+function age() {
+   
+    let currentYear = date.getFullYear();
 
+<<<<<<< HEAD
+    let birthYear = bYear.valueAsDate.getFullYear();
+    
+    return age = currentYear - birthYear;
+    
+}
+=======
 
 let year = bYear.valueAsDate
 let birthYear = year.getFullYear();
@@ -21,5 +30,10 @@ function answer(){
     key.innerText = `My name is ${firstName.value} ${lastName.value} and I'm ${age} years old.
     I'm ${major.value} student. I study at ${uni.value} University. I'm interested in ${sub.value}.`
 };
+>>>>>>> 5a67dbec7f276313268843c186d53f2a190be757
 
-
+function answer(){
+    
+    key.innerText = `My name is ${firstName.value} ${lastName.value} and I'm ${age()} years old.
+    I'm ${major.value} student. I study at ${uni.value} University. I'm interested in ${sub.value}.`
+};
