80d176be76e7911da5dfe5f367efdf25ea74d6e0
Kyaw Lwin Soe <51574554+KyawLwinSoe27@users.noreply.github.com>
2022-01-09 18:44:57 +0630

    Update script.js

--- script.js
+++ script.js
@@ -13,24 +13,11 @@ function age() {
    
     let currentYear = date.getFullYear();
 
-<<<<<<< HEAD
     let birthYear = bYear.valueAsDate.getFullYear();
     
     return age = currentYear - birthYear;
     
 }
-=======
-
-let year = bYear.valueAsDate
-let birthYear = year.getFullYear();
-
-let age = currentYear - birthYear;
-
-function answer(){
-    key.innerText = `My name is ${firstName.value} ${lastName.value} and I'm ${age} years old.
-    I'm ${major.value} student. I study at ${uni.value} University. I'm interested in ${sub.value}.`
-};
->>>>>>> 5a67dbec7f276313268843c186d53f2a190be757
 
 function answer(){
     
